3b94b652c3141e88688070eda775c95dd6ebd6bf
Harsh Pareek <32735763+Harsh-Pareek-Commits@users.noreply.github.com>
2021-05-28 00:23:00 +0530

    Add files via upload

---
+++ cmc/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+  <div style="height:50px; border-bottom-width: 3px; border-bottom-style: solid; border-bottom-color: red;">
+<h1>Door 2 Door</h1>
+</div><br>
+<div style="height:70px; border-width: 3px; border-style: solid; border-color: green;background: #016937;
+background: -webkit-linear-gradient(top left, #016937, #0F7FB0);
+background: -moz-linear-gradient(top left, #016937, #0F7FB0);
+background: linear-gradient(to bottom right, #016937, #0F7FB0);">
+  <center><h1 style="color: white">Courier Services</h1></center>
+
+</div>
+<div style="height:270px; border-width: 3px; border-style: solid; border-color: #016937;padding: 30px">
+  <p>City economy has undergone rapid growth thanks largely to its technology industry. As a result, many businesses in Bangalore find they have a need to get packages across the country on a regular basis.</p><p>
+
+Shipping a parcel doesn't have to be tiresome or complex task and if you make use of the services of PickParcel in Bangalore, you will quickly see that sending a courier from anywhere in Karnataka is much easier than you ever thought.</p><p>
+
+For those who need guarantee that their parcel reaches its destination safely and on time, using a courier service at PickParcel is really the only option. We are India's first and leading online parcel booking service, and the fact that we work alongside the reputed courier companies only means that every time you book a discounted courier service from Bangalore through us, you can rest assured that you will not only get the best of services, but it will come at a great price too.</p><p>
+
+At PickParcel we want our customers to book their services hassle-free, which is why our website is easy to navigate. Therefore, we have our Quick Quote form right at the homepage so you can enter you package details such as source, destination and weight, and we will provide you a FREE quote of list of couriers which offer the best price and service for you.</p><p>
+
+Shipping  is not only easy with us but its time and cost saving too. You can also keep track of shipment sent through us using our online tracking page. Trust PickParcel for you courier service needs.</p><p>
+</div>
+<br>
+<div style="width: 870px;margin-left: 325px;">
+  <div  style="display: inline-block;border-width: 3px; border-style: solid; border-color: #016937; float: left; border-radius: 30px; " >
+    <a href="login.php"><img width="400px" src="admin.gif" style="border-radius: 30px;">
+  </div></a>
+  <div height="180px" style="display: inline-block;border-width: 3px; border-style: solid; border-color: #016937; float: right;border-radius: 30px;">
+  <a href="track-status.php"><img  src="track.gif" style="border-radius: 30px;"></a>
+
+  </div>
+</div>
+<br><br><br>
+</body>
+</html>
---
+++ cmc/server.php
@@ -0,0 +1,7 @@
+<?php
+$dbserver="localhost";
+$dbusername="root";
+$dbpass="";
+$dbname="courier_db";
+$conn=mysqli_connect($dbserver, $dbusername, $dbpass, $dbname);
+
